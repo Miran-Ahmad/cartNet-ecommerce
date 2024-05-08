b35f39c914878a3b00606fefa20a386bee1234fa
Miran <222miran222@gmail.com>
2024-05-08 19:11:21 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { BrowserRouter } from "react-router-dom";
+
+export default function App() {
+  return (
+<main className="text-tertiary">
+  <BrowserRouter>
+    <Header/>
+  </BrowserRouter>
+</main>
+  )
+}
