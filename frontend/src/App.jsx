2169bfff74c03731a5f0083dfc6f727793f39d1c
Miran <222miran222@gmail.com>
2024-05-11 15:46:54 +0530

    product display

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,9 @@ import Home from "./pages/Home";
 import Category from "./pages/Category";
 import Product from "./pages/Product";
 import Footer from "./components/Footer";
+import clothingbanner from "./assets/clothingbanner.png";
+import electronicsbanner from "./assets/electronicsbanner.png";
+import cosmeticsbanner from "./assets/cosmeticsbanner.png";
 
 export default function App() {
   return (
@@ -12,9 +15,22 @@ export default function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/clothing" element={<Category />} />
-          <Route path="/cosmetics" element={<Category />} />
-          <Route path="/electronics" element={<Category />} />
+          <Route
+            path="/clothing"
+            element={<Category category={"clothing"} banner={clothingbanner} />}
+          />
+          <Route
+            path="/cosmetics"
+            element={
+              <Category category={"cosmetics"} banner={cosmeticsbanner} />
+            }
+          />
+          <Route
+            path="/electronics"
+            element={
+              <Category category={"electronics"} banner={electronicsbanner} />
+            }
+          />
           <Route path="/product" element={<Product />}>
             <Route path=":productId" element={<Product />} />
           </Route>
--- frontend/src/components/Item.jsx
+++ frontend/src/components/Item.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const Item = ({ id, name, image, old_price, new_price }) => {
   return (
-    <Link to={""} className="bg-white p-4 rounded-xl relative">
+    <Link to={`/product/${id}`} className="bg-white p-4 rounded-xl relative">
       <div className="flexCenter">
         <img src={image} alt="" height={211} width={211} className="rounded-lg drop-shadow-xl absolute bottom-44" />
       </div>
