266517e3163589cf6ddaace2a556325cc7f1e72a
Miran <222miran222@gmail.com>
2024-05-11 11:18:06 +0530

    New Arrivals

--- frontend/src/components/NewArrivals.jsx
+++ frontend/src/components/NewArrivals.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import LATEST from "../assets/latest";
+import Item from "./Item";
 
 const NewArrivals = () => {
   return (
-    <div>NewArrivals</div>
-  )
-}
+    <section className="max-padd-container bg-primary p-12 xl:py-28">
+      <div className="text-center max-w-xl mx-auto">
+        <h3 className="h3">New Arrivals</h3>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+      </div>
+      {/* container */}
+      <div className="grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6 gap-y-28 mt-32">
+        {LATEST.map((item) => (
+          <Item
+            key={item.id}
+            id={item.id}
+            name={item.name}
+            image={item.image}
+            new_price={item.new_price}
+            old_price={item.old_price}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
 
-export default NewArrivals
+export default NewArrivals;
