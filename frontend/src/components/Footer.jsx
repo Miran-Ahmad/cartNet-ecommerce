e48c3f2e33db335aaa5fac9e68216315b9028941
Miran <222miran222@gmail.com>
2024-05-11 11:54:05 +0530

    footer & home page done

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./Navbar";
+import SocialIcons from "./SocialIcons";
+import logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="max-padd-container bg-tertiary py-8">
+      <div className="flexCenter flex-col gap-y-4">
+        {/* logo */}
+        <Link to={"/"} className="flex items-center gap-x-2">
+          <img
+            src={logo}
+            alt="LogoImg"
+            height={31}
+            width={31}
+            className="mix-blend-multiply"
+          />
+          <span className="bold-24 hidden xs:flex text-white">CartNet</span>
+        </Link>
+        {/* nav */}
+        <div className="py-4">
+          <Navbar
+            containerStyles={
+              "flex gap-x-5 xl:gap-x-10 text-white medium-15 rounded-full px-2 py-1"
+            }
+          />
+        </div>
+        <SocialIcons />
+        <hr className="h-[1px] w-2/3 my-3" />
+        <div className="text-white">
+          Copyright &copy; CratNet | All Rights Reserved.
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
