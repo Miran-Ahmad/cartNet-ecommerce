9eceec22305a1a0ea9debf07d9d4365a36ac3f19
Miran <222miran222@gmail.com>
2024-05-31 22:32:35 +0530

    CRUD options added

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import Admin from './pages/Admin'
 
 const App = () => {
   return (
-    <div>App</div>
+    <main className='bg-primary text-tertiary'>
+      <div className='mx-auto max-w-[1500px]'>
+        <Navbar/>
+        <Admin/>
+      </div>
+    </main>
   )
 }
 
